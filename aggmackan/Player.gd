ede044fd5a0bdb4d0115bc8a353e0d64db96447a
eliasjohansson <eljoh633@student.liu.se>
2021-11-27 11:41:28 +0100

    added player animation

--- aggmackan/Player.gd
+++ aggmackan/Player.gd
@@ -1,11 +1,17 @@
 extends KinematicBody2D
 
 
-var speed = 250
+var speed = 150
 var has_egg : bool = false
+var dodge_speed = 400
+var dodge_time = 0.1
 
 var score = 0
 var vel : Vector2 = Vector2()
+var dodge_vel = Vector2()
+onready var dodge_timer = get_node("Timer")
+onready var sprite = get_node("AnimatedSprite")
+onready var dodge_emmiter = get_node("dodge_par")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -26,6 +32,33 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		vel.x = speed
 
+	if Input.is_action_just_released("dodge"):
+		dodge_emmiter.rotation_degrees = 0
+		dodge_emmiter.rotate(vel.angle()+PI/2)
+		dodge_timer.start(dodge_time)
+		vel = vel.normalized()*dodge_speed
+		dodge_vel = vel
+		
+	
+	if dodge_timer.time_left != 0:
+		vel = dodge_vel
+		dodge_emmiter.emitting = true
+	else:
+		dodge_emmiter.emitting = false
+		dodge_vel = Vector2()
+		
+	if vel.length() > 0:
+		sprite.playing = true
+	else:
+		sprite.playing = false
+		sprite.frame = 0
+	
+	if vel.x > 0:
+		sprite.flip_h = false
+	elif vel.x < 0:
+		sprite.flip_h = true
+
+		
 	move_and_slide(vel, Vector2.UP)
 
 		
