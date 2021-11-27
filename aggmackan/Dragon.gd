e68b35e8205604c7427e90fbce2f218b62ecadc6
basvektorernas_bilder <basvektorernas_bilder@pop-os.localdomain>
2021-11-27 11:10:09 +0100

    Implemented AStar

--- aggmackan/Dragon.gd
+++ aggmackan/Dragon.gd
@@ -2,15 +2,42 @@ extends KinematicBody2D
 
 onready var player = get_parent().get_node("Player")
 var vel : Vector2 = Vector2()
+var astar = AStar.new()
+var pos
+var test = false
+
+enum Mode {
+	PATROL,
+	TRACK
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 	
 func _physics_process(delta):
-	vel.x = rand_range(-2, 2)
-	vel.y = rand_range(-2, 2)
-	move_and_collide(vel)
+	# Only for filthy testing
+	if (not test):
+		test = true
+		track()
+	
+func track():
+	pos = self.global_position
+	
+	# The dragons current position
+	astar.add_point(1, Vector3(0, 0, 0))
+	astar.add_point(2, Vector3(0, 1, 0))
+	astar.add_point(3, Vector3(1, 1, 0))
+	astar.add_point(4, Vector3(0, 0, 0))
+	
+	astar.connect_points(1, 2, false)
+	astar.connect_points(2, 3, false)
+	astar.connect_points(10, 3, false)
+	astar.connect_points(1, 4, false)
+	var res = astar.get_id_path(1, 3)
+	print (res)
+	
+	
 	
 
 
---
+++ aggmackan/TileMap.gd
@@ -0,0 +1,73 @@
+extends TileMap
+
+var astar
+var size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	astar = AStar2D.new()
+	# Get an estimate of the map size
+	size = get_used_rect().size
+	# Reserve a sufficient amount of space
+	astar.reserve_space(size.x * size.y)
+	initAStarMap()
+
+# Initialize the pathfinding algorithm
+func initAStarMap():
+	addAStarPoints()
+
+# Add every map tile to AStar
+func addAStarPoints():
+	for y in size.y:
+		for x in size.x:
+			var vector = Vector2(x, y)
+			var id = getAStarCellById(vector)
+			astar.add_point(id, map_to_world(vector))
+			print (get_cellv(vector))
+
+# Connect the points that are valid 
+func connectAStarPoints():
+	for y in size.y:
+		for x in size.x:
+			var id = getAStarCellById(Vector2(x, y))
+			
+			# Calculate every neighbour of the point
+			var neighbours = [
+				Vector2(x, y-1), Vector2(x, y+1),
+				Vector2(x-1, y), Vector2(x+1,y),
+				Vector2(x-1, y-1), Vector2(x-1, y+1), 
+				Vector2(x+1, y-1), Vector2(x+1, y+1)
+				]
+			
+			for neighbour in neighbours:
+				var idNeighbour = getAStarCellById(Vector2(x, y))
+				# Check if the point exists in the AStar mapping
+				if astar.has_point(idNeighbour):
+					# Connect the two points. This has to be changed
+					# for objects that are larger than one tile and cannot
+					# move too close to the edges.
+					astar.connect_points(id, idNeighbour, false)
+
+# Mark an AStar cell as occupied
+func occupyAStarCell(globalPos:Vector2):
+	changeCellStatus(globalPos, false)
+
+# Mark an AStar cell as unoccupied
+func freeAStarCell(globalPos:Vector2):
+	changeCellStatus(globalPos, true)
+
+# Change a cell's status to either occuped or unnocupied
+func changeCellStatus(globalPos:Vector2, state:bool):
+	var cell = self.world_to_map(globalPos)
+	var id = getAStarCellById(cell)
+	
+	if astar.has_points(id):
+		astar.set_point_disabled(state)
+
+func getAStarCellById(vCell:Vector2):
+	return vCell.y + (vCell.x * size.y)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
